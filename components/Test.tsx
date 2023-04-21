1383a3e571fe9dd48c3536c1fa0363497996489f
lshirase <lshirase@umich.edu>
2023-04-20 18:27:50 -0700

    test

--- components/Test.tsx
+++ components/Test.tsx
@@ -3,18 +3,28 @@ import { type Sketch } from "react-p5-wrapper";
 
 const sketch: Sketch = (p5) => {
   // eslint-disable-next-line no-param-reassign
-  p5.setup = () => p5.createCanvas(p5.windowWidth, p5.windowHeight, p5.WEBGL);
-
+  p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
+  const amount = 100;
   // eslint-disable-next-line no-param-reassign
   p5.draw = () => {
-    p5.background(250);
-    p5.normalMaterial();
-    p5.push();
-    p5.rotateZ(p5.frameCount * 0.01);
-    p5.rotateX(p5.frameCount * 0.01);
-    p5.rotateY(p5.frameCount * 0.01);
-    p5.plane(100);
-    p5.pop();
+    p5.background("#f1f1f1");
+    p5.fill(0);
+    p5.noStroke();
+    p5.translate(p5.width / 3, p5.height / 3);
+    for (let i = 0; i <= amount; i += 1) {
+      const y = p5.map(i, 0, amount, -p5.height * 0.6, p5.height * 0.6);
+      const x = p5.map(
+        p5.sin(p5.radians(p5.frameCount + i * 4)),
+        -1,
+        1,
+        -p5.width * 0.4,
+        p5.width * 0.4
+      );
+      p5.push();
+      p5.translate(x, y);
+      p5.ellipse(0, 0, 15, 15);
+      p5.pop();
+    }
   };
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,30 @@
 import Test from "components/Test";
+import { Meta } from "src/layouts/Meta";
+import { Main } from "src/templates/Main";
 
 const Index = () => {
   // const router = useRouter();
 
   return (
-    // <Main
-    //   meta={
-    //     <Meta
-    //       title="Next.js Boilerplate Presentation"
-    //       description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
-    //     />
-    //   }
-    // >
-    //   {/* <a href="https://github.com/ixartz/Next-js-Boilerplate">
-    //     <img
-    //       src={`${router.basePath}/assets/images/nextjs-starter-banner.png`}
-    //       alt="Nextjs starter banner"
-    //     />
-    //   </a> */}
+    <Main
+      meta={
+        <Meta
+          title="Next.js Boilerplate Presentation"
+          description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
+        />
+      }
+    >
+      {/* <a href="https://github.com/ixartz/Next-js-Boilerplate">
+        <img
+          src={`${router.basePath}/assets/images/nextjs-starter-banner.png`}
+          alt="Nextjs starter banner"
+        />
+      </a> */}
+      <Test />
+    </Main>
+    // <div className="mx-auto">
     //   <Test />
-    // </Main>
-    <Test />
+    // </div>
   );
 };
 
